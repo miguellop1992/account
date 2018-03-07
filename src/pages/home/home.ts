974b2f01ba8d7ed5224e16284d45b0368f9e4ca2
miguellop1992 <miguellop1992@gmail.com>
2018-03-06 20:14:00 -0400

    *Cambio en la interface de home *Agregado en el menu Lista de deudas *Agregado el campo de moneda

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { IonicPage, NavController,  AlertController, Alert,Platform } from 'ionic-angular';
 import { AccountPage } from '../../pages/account/account';
 import { OperationPage } from '../../pages/operation/operation';
 import { AccountProvider, IAccount } from '../../providers/account.provider';
@@ -15,15 +15,25 @@ export class HomePage {
 
   static title: string = "Cuentas";
   private list: IAccount[] = [];
+  // private list: IAccount[] = [{
+  //   balance:400000000000,
+  //   name: "Pago Mami",
+  //   coin: "$"
+  // }];
 
-  constructor(public navCtrl: NavController, private accProv: AccountProvider, public plt: Platform) {
+  constructor(public navCtrl: NavController, private accProv: AccountProvider, private alertCtrl: AlertController,public plt: Platform) {
 
   }
 
   ionViewWillEnter() {
     this.plt.ready().then(() => {
-      this.accProv.getAll().then(data => this.list = data);
+      this.load();
     })
+  }
+
+  load(){
+    
+    this.accProv.getAll().then(data => this.list = data);
   }
 
   get title() {
@@ -36,5 +46,29 @@ export class HomePage {
 
   add() {
     this.navCtrl.push(AccountPage);
+  }
+
+  delete(acc) {
+    
+    let alert = this.alertCtrl.create({
+      title: 'Advertencia',
+      subTitle: '¿Esta seguro que desea eliminar esta cuenta?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: data => {
+            this.accProv.delete(acc._id).then(data => {
+              if (data) {
+                this.load();
+              }
+            })
+          }
+        }, {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
+    });
+    alert.present();
   }
 }
