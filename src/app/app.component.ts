b412d5c2747e863457a135829f2ee34f1759f563
miguellop1992 <miguellop1992@gmail.com>
2018-03-16 16:15:32 -0400

    Ediccion para cambio de nombre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      
     });
   }
 
